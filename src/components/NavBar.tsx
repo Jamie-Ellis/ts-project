2f6053ccdb5f484f487226240284695bad1eb57c
Jamie <jamiekyaellis@gmail.com>
2021-05-27 17:00:28 +0200

    up

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+class NavBar extends Component<{}, {}> {
+  render() {
+    return (
+      <Navbar>
+        <Container>
+          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default NavBar;
